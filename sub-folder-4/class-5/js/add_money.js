d1eb1501e0836d64618420d794e63385414720ef
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-09-15 23:11:29 +0600

    sub-folder-4 (class-3)

---
+++ sub-folder-4/class-5/js/add_money.js
@@ -0,0 +1,32 @@
+// alert();
+
+document.querySelector('#add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    let amount = getInputValueByID('amount');
+    // console.log(amount);
+    let pin = getInputValueByID('pin-number');
+    // console.log(amount, pin);
+    let account = document.getElementById('account-number').value;
+    // console.log(account);
+
+    if(account.length === 11){
+        // console.log('Account Okay');
+        if(pin === 1111){
+            // console.log('Pin Okay');
+
+            const currentBalance = getInnerTextByID('total-balance');
+            // console.log(currentBalance);
+
+            const sum = amount + currentBalance;
+            // document.getElementById('total-balance').innerText = sum;
+
+            setInnerTextByID_Value('total-balance', sum);
+        }else{
+            console.log('Pin not matched!');
+        }
+    }else{
+        console.log('Invalid Account')
+    }
+
+});
