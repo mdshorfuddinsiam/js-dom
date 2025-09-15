52565c94817cb147cee175b316808372da923f5e
mdshorfuddin <mdshorfuddinsiam6660@gmail.com>
2025-09-16 00:26:47 +0600

    sub-folder-4 (class-7)

---
+++ sub-folder-4/class-7/js/cash_out.js
@@ -0,0 +1,20 @@
+document.getElementById('cash-out-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const account = document.getElementById('cash-out-account-number').value;
+
+    const pin = getInputValueByID('cash-out-pin-number');
+    const amount = getInputValueByID('cash-out-amount');
+
+    if(account.length === 4){
+        if(pin === 1111){
+            const balance = getInnerTextByID('total-balance');
+            const updatedBalance = balance - amount;
+            setInnerTextByID_Value('total-balance', updatedBalance);
+        }else{
+            console.log('Wrong Pin');
+        }
+    }else{
+        console.log('Wrong Account');
+    }
+});
